0ecbc015c2ecb303f445aa5bd1a42a5cb90ec45a
zhanghao <haozhang6868@163.com>
2024-01-07 20:54:25 +0800

    a practice

---
+++ elif多条件判断.py
@@ -0,0 +1,9 @@
+height = int(input("please input your height:"))
+vipleve = int(input("please input your vip leve:"))
+180
+if height < 120 :
+    print('free')
+elif vipleve > 3 :
+    print('free')
+else :
+    print("sorry")
