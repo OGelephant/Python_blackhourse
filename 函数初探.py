59d28e2f0eca61a329092233eb8cbac6cb71905e
zhanghao <haozhang6868@163.com>
2024-01-09 21:19:20 +0800

    20204-01-09Study recoading

--- 函数初探.py
+++ 函数初探.py
@@ -2,3 +2,13 @@ def say_hello ():
     print("Hello world!")
 
 say_hello()
+
+x=input ()
+
+def temp (x):
+    if float(x) > 37.5:
+        print("you have a cold!")
+    else:
+        print("you are good!")
+
+temp(x)
